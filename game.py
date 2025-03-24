adb1358ff157b2de6dec92efb5d890b433abc8b3
jobbiecivanne <jobbie.civanne@gmail.com>
2025-03-24 13:56:15 -0500

    Add files via upload

---
+++ game.py
@@ -0,0 +1,317 @@
+
+girl = r'''
+       :|
+       :|           . ,
+       :|           _)/'/
+       :|         ,/'/,'<,
+       :|       .-'\\_.-`
+       :|      ///'\\
+       .|      |e^e-?
+      (/\      `.=,'._    .
+      `: L   _.-J   L '\.' `.
+       :|/`.'  / \ /'_.'     \
+       :|\   .-\__V__<    \/  L
+       :| `-'   \  ,  \   /\  |
+       :|        \._.=-\.     F
+       :|        )\_/   `-.__J
+       :|       /  |    |
+       .|      /   |   /
+ ____  :|___  (   <|' | _______________________________________
+       :|      \  '\  \                     ~
+   ~   :|       `. |   |  ~          ___..._          ~
+       :|         `J  /   ...--..---'     _ ""------......____
+       :|     ,    _)_|>.__,_            c")_
+-.b'ger '-.,-.+- cC\\_\  "'-=-i"-.       (__/
+  VK/a:f         _,. -
+'''
+
+temple = r'''  V        V    	
+                          +........+
+                          J||||||||L
+                        /            \
+                       ~/~~~~~~~~~~~~\~
+                       /              \
+                    ~~/~~~~~~~~~~~~~~~~\~~
+        _--^I^--_    /                  \
+       / /^~|~^\ \~~~~~~+~+~~~~~~~~+~+~~~~~
+      / /   |   \ \     | |        | |
+      | |   |   | |     = =        = =
+~~~~~~~ |   |o  | ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+------- |   |   | ---------------------------------
+------- |   |   | ---------------------------------
+======= |===|===| ================================ '''
+
+island = r'''                                           ____
+                                         v        _(    )
+        _ ^ _                          v         (___(__)
+       '_\V/ `
+       ' oX`
+          X                            v
+          X             
+          X                                                .
+          X                                                |\
+          X.a##a.                                          |_\
+       .aa########a.>>                                    __|__
+    .a################aa.                                 \   /
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'''
+
+hall = r'''                                           ____
+    _________________________________________________________
+ /|     -_-                                             _-  |\
+/ |_-_- _                                         -_- _-   -| \   
+  |                            _-  _--                      | 
+  |                            ,                            |
+  |      .-'````````'.        '(`        .-'```````'-.      |
+  |    .` |           `.      `)'      .` |           `.    |          
+  |   /   |   ()        \      U      /   |    ()       \   |
+  |  |    |    ;         | o   T   o |    |    ;         |  |
+  |  |    |     ;        |  .  |  .  |    |    ;         |  |
+  |  |    |     ;        |   . | .   |    |    ;         |  |
+  |  |    |     ;        |    .|.    |    |    ;         |  |
+  |  |    |____;_________|     |     |    |____;_________|  |  
+  |  |   /  __ ;   -     |     !     |   /     `'() _ -  |  |
+  |  |  / __  ()        -|        -  |  /  __--      -   |  |
+  |  | /        __-- _   |   _- _ -  | /        __--_    |  |
+  |__|/__________________|___________|/__________________|__|
+ /                                             _ -        lc \
+/   -_- _ -             _- _---                       -_-  -_ \
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'''
+
+gameover = r'''                                           ____
+  ******GAME OVER******
+                 __
+               /`__`\
+           .=.| ('') |.=.
+          /.-.\ _)(_ /.-.\
+         |:    / ~~ \    :|
+         \ :  | (__) |  : /
+          | :  \_/\_/  : |
+          |:  /|    |\  :|
+          \_/` |    | `\_/
+               |    |
+               |    |
+          jgs  |~~~~|
+               '----'''
+
+fish = r'''                                           ____
+  ,---,
+  _    _,-'    `--,
+ ( `-,'            `\
+  \           ,    o \
+  /   ,       ;       \
+ (_,-' \       `, _  ""/
+     pb `-,___ =='__,-'''
+
+booty = r'''                                           ____
+  ,
+          /     \
+         (   Y   )
+          \  |  /'''
+
+boat = r'''    
+     __/\__
+  ~~~\____/~~~~~~
+    ~  ~~~   ~.   
+    '''
+
+sleep = r'''    
+     _   z
+                              Z
+                    .--.  Z Z
+                   / _(c\   .-.     __
+                  | / /  '-;   \'-'`  `\______
+                  \_\/'/ __/ )  /  )   |      \--,
+                  | \`""`__-/ .'--/   /--------\  \
+                   \\`  ///-\/   /   /---;-.    '-'
+             jgs                (________\  \
+                                          '-' 
+    '''
+snu_snu = r'''    
+     ,        ,
+           \y ,--.y/
+           /,---. )\
+          / (((\\)\ \
+          \ \\-_/ / /
+           \ i   i /
+            (_)=(_)
+             ) . (
+            /\---/\
+           /  )-(  \
+          /  /   \  \
+         / ,"     ". \
+        / /         \ \
+  hjw  /-)           (-\
+      / ^!           !^ \
+
+    '''
+rope = r'''    
+,        ,
+   \\//
+    //
+   //\\ 
+   \\//
+    //
+   //\\
+   \\//
+    //
+   //\\
+   \\//
+    //
+   //\\ 
+   \\//
+    //
+   //\\
+   \\//
+    //
+   //\\
+
+    '''
+signature = r'''    
+  .-.  
+    (  |.-.
+   .-\/__ )     
+  (__/ \
+     \_/
+ 
+    
+     .-.            .-.
+    (  |.-.      .-.|  ) 
+   .-'/__ )     ( __\ '-.
+  (__/ \           / \___)
+     \_/           \_/
+
+    '''
+
+axe = r'''    
+  T  <|>  (|||)   . .    _   _     .   .
+    |     |    ( | )  / \|/ \  .` \_/ `.    __
+          |     `|`   \_/|\_/  .   #   .  .`  `//``.
+                 |       |      `./#\.`   .   //   .
+                 |       |         #       ``//\_.`
+                         |         #        //
+                                   #       //
+                                   #      //
+    '''
+
+
+#**********************
+print(f"*********\nWelcome to Treasure Island.\nCan you escape the and find the treasure\n"
+      f"or will you get caught and forced to Snu-Snu?\n"
+      f"{ girl }")
+print(f"******\n Your boat docks onto the island. After crawling through snakes and tall plants\n " + island)
+print("You come across an ancient structure and decide to go in\n" + temple)
+print("To your left there is darkness, but to your right you see a glimmer of light\n")
+print(hall)
+
+direction = (input("Which direction do you want to go? L or R\n")).strip().upper()
+if direction == "L":
+    print("Your bravery saved you and you were able to venture further into the temple")
+    print("As you venture further into the temple you hear running water. Good because you are thristy.\n"
+          "You take a drink from the water and everything is fine. As you come up for air you hear\n"
+          "Footsteps behind you! The natives! You need to make a decision soon.\n"
+          "The water is safe enough to drink so you could swim, or you can hide in the dark behind the rubble.")
+    hide = input("Do you swim in the water or hide in trash? HIDE SWIM \n").strip().upper()
+    if hide == "HIDE":
+        print(f"A tall woman walks into the room. You hold your breath with fear and because this trash STINKS\n"
+              f"{girl}\n"
+              f"She lingers in the room for awhile before ultimately deciding to leave. As she walks away you get\n"
+              f"a glimps of her butt.{booty} it's plump, like the developer's")
+        print(f"{boat} You are snapped out of daydreaming about the dev by a boat drifting towards you.\n "
+              f"checking that the coast is clear, you run out and climb aboard. You fall asleep\n"
+              f"{sleep} \n you are awaken by the thud of you making it back to land. You are in another area\n "
+              f"of the temple.{hall} You reach 3 doors. Blue Black and Gold.\n"
+              f"There is no sound coming from either, but you hear footsteps behind you, the giant woman!\n"
+              f"you have to make a decision soon")
+        door = (input("No time to think! What color door do you pick? BLACK BLUE or GOLD\n")).strip().upper()
+        if door == "BLUE":
+            print(f"You find yourself in a room with a rope coming from a window. Not being able to trust\n"
+                  f"your judgement you hesitate to climb it but the women are getting closer{rope}\n"
+                  f"With no time to lose you climb. As you pull yourself out of the window the rope slips from\n"
+                  f"under you. PHEW! Barely made it. You slip though the jungle back to your ship\n"
+                  f"Congratulations! No treasure but you live to see another adventure\n"
+                  f"{island}\n"
+                  f"Story and code by Jobba Wobba, art found at ascii.co.uk/art {signature}")
+
+        elif door == "GOLD":
+            print(f"You enter a room finally thinking you are safe. Surrounded by jewels and gold you began to imagine\n"
+                  "your life of riches and status! You flop on the bed exhausted from your journey. As a sense\n "
+                  "of calmness comes over your body BAM! The door swings open. \n "
+                  f"time for snu snu {snu_snu}\n"
+                  f"{gameover}")
+        else:
+            print(f"You run into the room locking it behind you. You lean against the wall to catch your breath\n"
+                  "But unbeknowingstly your shoulder pressed against a button on the wall. \n"
+                  "who the hell designed this place?! Another door opens and you are sick of doors\n"
+                  f"but you walk towards it ready to continue to the treasure when {axe}\n"
+                  f"A giant axe swings towards you. You died\n"
+                  f"{gameover}")
+
+    else:
+            print(f"{fish}\n A swarm of demon fish attack you. As you scream through the pain the last thing you see\n"
+              f"is a large beautiful woman looking at you{gameover}")
+elif direction == "R":
+    print("As you ventured towards the light you carelessly fell into a hole.\n Pierced by spikes you have died")
+    print(gameover)
+
+    # **********************
+    # print(f"*********\nWelcome to Treasure Island.\nCan you escape the and find the treasure\n"
+    #       f"or will you get caught and forced to Snu-Snu?\n"
+    #       f"{girl}")
+    # print(f"******\n Your boat docks onto the island. After crawling through snakes and tall plants\n " + island)
+    # print("You come across an ancient structure and decide to go in\n" + temple)
+    # print("To your left there is darkness, but to your right you see a glimmer of light\n")
+    # print(hall)
+    #
+    # direction = (input("Which direction do you want to go? L or R\n")).strip().upper()
+    # if direction == "L":
+    #     print("Your bravery saved you and you were able to venture further into the temple")
+    #     print("As you venture further into the temple you hear running water. Good because you are thristy.\n"
+    #           "You take a drink from the water and everything is fine. As you come up for air you hear\n"
+    #           "Footsteps behind you! The natives! You need to make a decision soon.\n"
+    #           "The water is safe enough to drink so you could swim, or you can hide in the dark behind the rubble.")
+    #     hide = input("Do you swim in the water or hide in trash? HIDE SWIM \n").strip().upper()
+    #     if hide == "HIDE":
+    #         print(f"A tall woman walks into the room. You hold your breath with fear and because this trash STINKS\n"
+    #               f"{girl}\n"
+    #               f"She lingers in the room for awhile before ultimately deciding to leave. As she walks away you get\n"
+    #               f"a glimps of her butt.{booty} it's plump, like the developer's")
+    #         print(f"{boat} You are snapped out of daydreaming about the dev by a boat drifting towards you.\n "
+    #               f"checking that the coast is clear, you run out and climb aboard. You fall asleep\n"
+    #               f"{sleep} \n you are awaken by the thud of you making it back to land. You are in another area\n "
+    #               f"of the temple.{hall} You reach 3 doors. Blue Black and Gold.\n"
+    #               f"There is no sound coming from either, but you hear footsteps behind you, the giant woman!\n"
+    #               f"you have to make a decision soon")
+    #         door = (input("No time to think! What color door do you pick? BLACK BLUE or GOLD\n")).strip().upper()
+    #         if door == "BLUE":
+    #             print(f"You find yourself in a room with a rope coming from a window. Not being able to trust\n"
+    #                   f"your judgement you hesitate to climb it but the women are getting closer{rope}\n"
+    #                   f"With no time to lose you climb. As you pull yourself out of the window the rope slips from\n"
+    #                   f"under you. PHEW! Barely made it. You slip though the jungle back to your ship\n"
+    #                   f"Congratulations! No treasure but you live to see another adventure\n"
+    #                   f"{island}\n"
+    #                   f"Story and code by Jobba Wobba, art found at ascii.co.uk/art {signature}")
+    #
+    #         elif door == "GOLD":
+    #             print(
+    #                 f"You enter a room finally thinking you are safe. Surrounded by jewels and gold you began to imagine\n"
+    #                 "your life of riches and status! You flop on the bed exhausted from your journey. As a sense\n "
+    #                 "of calmness comes over your body BAM! The door swings open. \n "
+    #                 f"time for snu snu {snu_snu}\n"
+    #                 f"{gameover}")
+    #         else:
+    #             print(f"You run into the room locking it behind you. You lean against the wall to catch your breath\n"
+    #                   "But unbeknowingstly your shoulder pressed against a button on the wall. \n"
+    #                   "who the hell designed this place?! Another door opens and you are sick of doors\n"
+    #                   f"but you walk towards it ready to continue to the treasure when {axe}\n"
+    #                   f"A giant axe swings towards you. You died\n"
+    #                   f"{gameover}")
+    #
+    #     else:
+    #         print(f"{fish}\n A swarm of demon fish attack you. As you scream through the pain the last thing you see\n"
+    #               f"is a large beautiful woman looking at you{gameover}")
+    # elif direction == "R":
+    #     print("As you ventured towards the light you carelessly fell into a hole.\n Pierced by spikes you have died")
+    #     print(gameover)
+
+
+
